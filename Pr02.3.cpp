016a2b2d71227f8df46728ca0e812e55a1c94732
Nilay-Patel-5 <nilaypatel0505@gmail.com>
2025-02-18 23:07:14 +0530

    Update Pr02.3.cpp

--- Pr02.3.cpp
+++ Pr02.3.cpp
@@ -1,14 +1,13 @@
-
 #include<iostream>
 using namespace std;
 
 class Account
 {
-   
-    string AccHolderName;
+
+    char AccHolderName[10];
     long int AccNum,Balance;
-   
-    public:
+
+public:
     void AddAccDetails()
     {
         cout<<"Enter the account holder's name: ";
@@ -18,14 +17,9 @@ class Account
         Balance=1000;
     }
 
-    int GetAccNum()
-    {
-        return AccNum;
-    }
-
     void DepositMoney()
     {
-        
+
         long Amount;
 
         cout<<"Enter the amount to be deposit: ";
@@ -41,9 +35,9 @@ class Account
         cout<<"Enter the amount to be withdraw: ";
         cin>>Amount;
 
-        if(Amount>=Balance)
+        if(Amount>=Balance-1000)
         {
-                cout<<"Insufficient balance"<<endl;
+            cout<<"Insufficient balance"<<endl;
         }
 
         else
@@ -66,110 +60,103 @@ int main()
     class Account Acc[100];
     long NumAcc=0,AccFind;
     int Choice;
-    
+
     cout<<"Enter 1 to add account."<<endl;
     cout<<"Enter 2 to deposit."<<endl;
     cout<<"Enter 3 to withdraw."<<endl;
     cout<<"Enter 4 to display the current balance."<<endl;
     cout<<"Else to Exit."<<endl;
-    
-    next : cout<<"Enter the option which you want: ";
-           cin>>Choice;
-
-    for(int i=0;i<5;i++)
-    {
-        Acc[i].AddAccDetails();
-    }
-
     cout<<"=================================="<<endl;
-    
-    while(Choice != 0)
+
+
+next :
+    cout<<"Enter the option which you want: ";
+    cin>>Choice;
+
+    switch(Choice)
     {
-       switch(Choice)
+    case 1:
+    {
+        for(int i=0; i<=NumAcc; i++)
         {
-            case 1:
-            {
-                for(int i=0;i<=NumAcc;i++)
-                {
-                    Acc[NumAcc].AddAccDetails();
-                    NumAcc++;
-                    break;
-                }
-            }
+            Acc[NumAcc].AddAccDetails();
+            NumAcc++;
             break;
-            
-            case 2:
-                {
-                    cout<<"Enter the account no.: ";
-                    cin>>AccFind;
-
-                    for(int i=0;i<NumAcc;i++)
-                    {
-                        if(AccFind<=NumAcc)
-                        {
-                            Acc[NumAcc-1].DepositMoney();
-                            break;
-                        }
-
-                        else
-                        {
-                            cout<<"Account no. not found"<<endl;
-                            break;
-                        }
-                    }
-                    break;
-                }
-
-                case 3:
-                {
-                    cout<<"Enter the account no.: ";
-                    cin>>AccFind;
-
-                    for(int i=0;i<NumAcc;i++)
-                    {
-                        if(AccFind<=NumAcc)
-                        {
-                            Acc[NumAcc-1].WithdrawMoney();
-                            break;
-                        }
-
-                        else
-                        {
-                            cout<<"Account no. not found"<<endl;
-                            break;
-                        }
-                    }
-                    break;
-                }
-            case 4:
-                {
-                    cout<<"Enter the account no.: ";
-                    cin>>AccFind;
-
-                    for(int i=0;i<5;i++)
-                    {
-                        if(AccFind<=NumAcc)
-                        {
-                            Acc[NumAcc-1].DisplayAccDetails();
-                            break;
-                        }
-
-                        else
-                        {
-                            cout<<"Account no. not found"<<endl;
-                            break;
-                        }
-                    }
-                    break;
-                    }
-
-            default:
-                {
-                    cout<<"24CE089_NILAY PATEL";
-                    return 0;
-                }
-            }
-            goto next;
+        }
+        break;
     }
-}
 
+    case 2:
+    {
+        cout<<"Enter the account no.: ";
+        cin>>AccFind;
+
+        for(int i=0; i<NumAcc; i++)
+        {
+            if(AccFind<=NumAcc)
+            {
+                Acc[NumAcc-1].DepositMoney();
+                break;
+            }
+
+            else
+            {
+                cout<<"Account no. not found"<<endl;
+                break;
+            }
+        }
+        break;
+    }
+
+    case 3:
+    {
+        cout<<"Enter the account no.: ";
+        cin>>AccFind;
+
+        for(int i=0; i<NumAcc; i++)
+        {
+            if(AccFind<=NumAcc)
+            {
+                Acc[NumAcc-1].WithdrawMoney();
+                break;
+            }
+
+            else
+            {
+                cout<<"Account no. not found"<<endl;
+                break;
+            }
+        }
+        break;
+    }
+
+    case 4:
+    {
+        cout<<"Enter the account no.: ";
+        cin>>AccFind;
+
+        for(int i=0; i<5; i++)
+        {
+            if(AccFind<=NumAcc)
+            {
+                Acc[NumAcc-1].DisplayAccDetails();
+                break;
+            }
+
+            else
+            {
+                cout<<"Account no. not found"<<endl;
+                break;
+            }
+        }
+        break;
+    }
+
+    default:
+    {
+        cout<<"24CE089_NILAY PATEL";
+        return 0;
+    }
+    }
+    goto next;
+}
