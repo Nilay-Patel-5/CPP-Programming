05f8b33613116d3463373bd3cd2896cd0749fd8b
Nilay-Patel-5 <nilaypatel0505@gmail.com>
2025-03-21 22:04:47 +0530

    Add files via upload

---
+++ Pr04.2.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+class Person
+{
+    char Name[10];
+    int Age;
+
+public:
+
+    Person()
+    {
+        strcpy(Name,"N/A");
+        Age = 0;
+    }
+
+    void AddPersonData()
+    {
+        cout << "Enter Name: ";
+        cin >> Name;
+        cout << "Enter Age: ";
+        cin >> Age;
+    }
+
+    void DisplayPersonData()
+    {
+        cout << "Name: " << Name << endl;
+        cout << "Age: " << Age << endl;
+    }
+};
+
+class Employee : private Person
+{
+    int EmployeeID;
+
+public:
+
+    Employee() : Person()
+    {
+        EmployeeID = 0;
+    }
+
+    void AddEmployeeData()
+    {
+        AddPersonData();
+
+        cout << "Enter employee ID: ";
+        cin >> EmployeeID;
+    }
+
+    void DisplayEmployeeData()
+    {
+        DisplayPersonData();
+
+        cout << "Employee ID: " << EmployeeID << endl;
+    }
+};
+
+class Manager : private Employee
+{
+    char Department[10];
+
+public:
+
+    Manager() : Employee()
+    {
+        strcpy(Department,"N/A");
+    }
+
+    void AddManagerData()
+    {
+        AddEmployeeData();
+
+        cout << "Enter department: ";
+        cin >> Department;
+    }
+
+    void DisplayManagerData()
+    {
+        DisplayEmployeeData();
+
+        cout << "Department: " << Department << endl;
+    }
+};
+
+int main()
+{
+    class Manager M;
+
+    M.AddManagerData();
+    M.DisplayManagerData();
+
+    return 0;
+}
