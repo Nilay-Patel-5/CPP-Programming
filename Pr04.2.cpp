0efa2c176f9476a1c353a2ecf9c5a3d963c8f939
Nilay-Patel-5 <nilaypatel0505@gmail.com>
2025-03-25 21:31:24 +0530

    Update Pr04.2.cpp

--- Pr04.2.cpp
+++ Pr04.2.cpp
@@ -1,96 +1,108 @@
-#include<iostream>
-#include<cstring>
+//This program is prepared by 24CE089_NILAY PATEL.
+#include<iostream>//Standard input/output library
+#include<cstring>//Standard string library
 
 using namespace std;
 
-class Person
+class Person//Creating class
 {
+    //Private data-members
     char Name[10];
     int Age;
 
+//Making public functions for different tasks
 public:
 
-    Person()
+    Person()//Constructor of Person class
     {
-        strcpy(Name,"");
+        //Initializing data members with default value
+        strcpy(Name," ");
         Age = 0;
     }
 
-    void AddPersonData()
+    void AddPersonData()//Function for Adding person's data
     {
         cout << "Enter Name: ";
-        cin >> Name;
+        cin >> Name;//Taking input of Name
         cout << "Enter Age: ";
-        cin >> Age;
+        cin >> Age;//Taking input of age
     }
 
-    void DisplayPersonData()
+    void DisplayPersonData()//Function for displaying person's data
     {
-        cout << "Name: " << Name << endl;
-        cout << "Age: " << Age << endl;
+        cout << "Name: " << Name << endl;//Displaying Name
+        cout << "Age: " << Age << endl;//Displaying Age
     }
 };
 
-class Employee : private Person
+class Employee : private Person//Creating class with a parent class
 {
+    //Private data-members
     int EmployeeID;
 
+//Making public functions for different tasks
 public:
 
-    Employee() : Person()
+    Employee() : Person()//Constructor of Person class with a constructor of parent
     {
+        //Initializing data members with default value
         EmployeeID = 0;
     }
 
-    void AddEmployeeData()
+    void AddEmployeeData()//Function for displaying employee's data
     {
-        AddPersonData();
+        AddPersonData();//Calling AddPersonData function internally
 
         cout << "Enter employee ID: ";
-        cin >> EmployeeID;
+        cin >> EmployeeID;//Taking input of employee ID
     }
 
-    void DisplayEmployeeData()
+    void DisplayEmployeeData()//Function for displaying employee's data
     {
-        DisplayPersonData();
+        DisplayPersonData();//Calling DisplayPersonData function internally
 
-        cout << "Employee ID: " << EmployeeID << endl;
+        cout << "Employee ID: " << EmployeeID << endl;//Displaying employee's ID
     }
 };
 
-class Manager : private Employee
+class Manager : private Employee//Creating class with a parent class
 {
+    //Private data-members
     char Department[10];
 
+//Making public functions for different tasks
 public:
 
-    Manager() : Employee()
+    Manager() : Employee()//Constructor of Person class with a constructor of parent
     {
-        strcpy(Department,"");
+        //Initializing data members with default value
+        strcpy(Department," ");
     }
 
-    void AddManagerData()
+    void AddManagerData()//Function for displaying manager's data
     {
-        AddEmployeeData();
+        AddEmployeeData();//Calling AddEmployeeData function internally
 
         cout << "Enter department: ";
-        cin >> Department;
+        cin >> Department;//Taking input of  department
     }
 
-    void DisplayManagerData()
+    void DisplayManagerData()//Function for displaying Manager's data
     {
-        DisplayEmployeeData();
+        DisplayEmployeeData();//Calling DisplayEmployeeData function internally
 
-        cout << "Department: " << Department << endl;
+        cout << "Department: " << Department << endl;//Displaying department
     }
 };
 
-int main()
+int main()//Main function
 {
-    class Manager M;
+    class Manager M;//Creating object
 
-    M.AddManagerData();
-    M.DisplayManagerData();
+    M.AddManagerData();//Calling AddManagerData function
+    M.DisplayManagerData();//Calling DisplayManagerData function
 
-    return 0;
+    cout << "24CE089_NILAY PATEL." << endl;//Printing exiting message
+
+    return 0;//Return statement
 }
